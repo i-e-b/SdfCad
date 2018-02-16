618e142038fbcaa5c37007bda6b3f5ba201b2552
Iain Ballard <iain.ballard@gocompare.com>
2018-02-16 15:56:25 +0000

    edges

--- TkglTest/Tkgl/SliceToObject/Form1.cs
+++ TkglTest/Tkgl/SliceToObject/Form1.cs
@@ -10,6 +10,7 @@ namespace SliceToObject
             InitializeComponent();
             TestSlice();
         }
+        readonly int argbWhite = Color.White.ToArgb();
 
         private void TestSlice()
         {
@@ -17,24 +18,42 @@ namespace SliceToObject
                 outputPictureBox.Image.Dispose();
             }
             Bitmap input = new Bitmap(Image.FromFile("Example2.png"));
-            var argbWhite = Color.White.ToArgb();
             //using (var g = Graphics.FromImage(input))
             {
-                for (int y = 0; y < input.Height; y++)
+                for (int y = 1; y < input.Height - 1; y++)
                 {
-                    for (int x = 0; x < input.Width; x++)
+                    for (int x = 1; x < input.Width - 1; x++)
                     {
-                        var color = input.GetPixel(x,y);
-                        if (color.ToArgb() == argbWhite) {
+                        var color = input.GetPixel(x, y);
+                        if (color.ToArgb() == argbWhite)
+                        {
+                            //input.SetPixel(x, y, Color.Black);
+                        }
+                        else if (IsEdge(input, x, y))
+                        { // is an edge
                             input.SetPixel(x, y, Color.Black);
                         }
-                        else {
-                            input.SetPixel(x, y, Color.FromArgb(color.R, 0 /* green is up/down */, color.B));
+                        else
+                        {
+                            input.SetPixel(x, y, Color.FromArgb(
+                                127+color.R/2/*red is left/right*/,
+                                127 /* green is up/down */,
+                                127+color.B/2/*blue is front/back*/));
                         }
                     }
                 }
             }
             outputPictureBox.Image = input;
+        }
+
+        private bool IsEdge(Bitmap input, int x, int y)
+        {
+            if (input.GetPixel(x,y).ToArgb() == argbWhite) return false;
+            if (input.GetPixel(x-1,y).ToArgb() == argbWhite) return true;
+            if (input.GetPixel(x,y-1).ToArgb() == argbWhite) return true;
+            if (input.GetPixel(x+1,y).ToArgb() == argbWhite) return true;
+            if (input.GetPixel(x,y+1).ToArgb() == argbWhite) return true;
+            return false;
         }
     }
 }
